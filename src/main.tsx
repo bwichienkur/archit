8bff77ff09f5db4d0beff52b37675abe8770f954
bwichienkur <120682941+bwichienkur@users.noreply.github.com>
2026-08-30 08:43:43 -0400

    Load semantic review styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './cad/cad.css';
+import './semantic/semantic.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
