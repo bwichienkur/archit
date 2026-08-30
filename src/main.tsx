fedce2c155a7b62b3826000b6dc04c905628be4b
bwichienkur <120682941+bwichienkur@users.noreply.github.com>
2026-08-30 11:13:57 -0400

    Mount integrated Archit platform dock

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { PlatformDock } from './platform/PlatformDock';
 import './styles.css';
 import './cad/cad.css';
 import './semantic/semantic.css';
+import './platform/platform.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
+ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /><PlatformDock /></React.StrictMode>);
